53ff454bd97ac1a5761e7ff787d5c757d72edfd6
Ayon-exe <146752822+Ayon-exe@users.noreply.github.com>
2024-02-22 15:39:03 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
 function signin(){
 	document.getElementById("demo").innerHTML="Your username is: "+document.getElementById("username").value;
     	document.getElementById("demo1").innerHTML="Your username is: "+document.getElementById("password").value;
-	window.location.href = "homepage.html";
+	window.location.href = "./homepage.html";
 	}
