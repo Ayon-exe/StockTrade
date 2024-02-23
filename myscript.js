b2369f435ec1cfadf5eb74768a086b48704d0ea1
Ayon-exe <146752822+Ayon-exe@users.noreply.github.com>
2024-02-23 19:37:00 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -3,3 +3,14 @@ function signin(){
     	document.getElementById("demo1").innerHTML="Your username is: "+document.getElementById("password").value;
 	window.location.href = "./homepage.html";
 	}
+function generate(){
+    	document.getElementById("currprice").value= (Math.random() * 100).toFixed(2);
+	}
+    
+function buy(){
+    	document.getElementById("wallet").value=(document.getElementById("wallet").value)-(document.getElementById("currprice").value);
+    }
+    
+function sell(){
+    	(document.getElementById("wallet").value)= parseFloat(document.getElementById("currprice").value)+ parseFloat(document.getElementById("wallet").value);
+    }
