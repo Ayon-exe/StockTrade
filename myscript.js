303a94ca220dfa98b8533d8e20889f0d9d26b2d1
Ayon-exe <146752822+Ayon-exe@users.noreply.github.com>
2024-03-04 20:52:04 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -39,13 +39,8 @@ new Chart("myChart", {
   }
 });
 const container = document.getElementById('container');
-
 const registerBtn = document.getElementById('register');
-
 const loginBtn = document.getElementById('login');
-
-
-
 registerBtn.addEventListener('click', () => 
 {
     container.classList.add("active");
